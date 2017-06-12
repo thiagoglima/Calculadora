ef3cbc899656516dddd61445c0bcbff1306e5d20
Antonio Schiavon <aschiavon91@gmail.com>
2017-06-12 20:46:35 -0300

    Campos form

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,10 +1,24 @@
-var btnCasa = $('#btn-group-casa');
-var btnCarro = $('#btn-group-carro');
-$('#btn-casa').click(function(){
-  btnCasa.toggle();
-  btnCarro.toggle();
-});
-$('#btn-carro').click(function(){
-  btnCasa.toggle();
-  btnCarro.toggle();
+
+function get_values() {
+  var optObjeto = $('');
+  var optParcelas = $('');
+  var valorObj = $('');
+  var valorEmprestimo = $('');
+}
+
+$(document).ready(function(){
+  var btnCasa = $('#btn-group-casa');
+  var btnCarro = $('#btn-group-carro');
+  var labelValor = $('#label-valor-obj');
+
+  $('#btn-casa').click(function(){
+    btnCasa.show();
+    btnCarro.hide();
+    labelValor.html("Qual o valor estimado dessa casa?")
+  });
+  $('#btn-carro').click(function(){
+    btnCasa.hide();
+    btnCarro.show();
+    labelValor.html("Qual o valor estimado desse carro?")
+  });
 });
